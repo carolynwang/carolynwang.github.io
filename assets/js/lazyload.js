8b6bf5594fc9f6e3a3ce28575f7245aad0ea3810
jasonxuyang <jasonxuyang@gmail.com>
2022-08-07 10:11:51 -0700

    feature: test progressive image loading on artwork page

---
+++ assets/js/lazyload.js
@@ -0,0 +1,14 @@
+const images = document.querySelectorAll("img[data-src]");
+
+const loadImage = (event) => {
+  const image = event.currentTarget;
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+    image.removeEventListener("load", loadImage);
+  };
+};
+
+images.forEach((image) => {
+  image.addEventListener("load", loadImage);
+});
---
+++ assets/scripts/img-resize.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os, sys
+path = os.path.dirname(os.path.realpath(__file__))
+print(path)
+dirs = os.listdir( path )
+max_size = (100, 100)
+
+def resize_aspect_fit():
+    for item in dirs:
+        if item == '.DS_Store':
+            continue
+        if item == 'img-resize.py':
+            continue
+        if os.path.isdir(item):
+            continue
+        im = Image.open(path + "/" + item)
+        f, e = os.path.splitext(path + "/" + item)
+        im.thumbnail(max_size)
+        if im.mode in ("RGBA", "P"): im = im.convert("RGB")
+        im.save(f + '.jpg', 'JPEG', quality=90)
+resize_aspect_fit()
