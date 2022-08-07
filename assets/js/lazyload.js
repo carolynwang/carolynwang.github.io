36aa163e2776991e24bc9c7ce3ad3b2b6d9c01b9
jasonxuyang <jasonxuyang@gmail.com>
2022-08-07 10:29:31 -0700

    bug: fix some images fail to load

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -1,13 +1,16 @@
 const images = document.querySelectorAll("img[data-src]");
 
+const cleanup = (event) => {
+  const image = event.currentTarget;
+  image.removeAttribute("data-src");
+  image.classList.remove("placeholder");
+};
+
 const loadImage = (event) => {
   const image = event.currentTarget;
   image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-    image.removeEventListener("load", loadImage);
-    image.classList.remove("placeholder");
-  };
+  image.removeEventListener("load", loadImage);
+  image.addEventListener("load", cleanup);
 };
 
 images.forEach((image) => {
