7524649280832d6189da507d7fa6bd8e28bec35d
jasonxuyang <jasonxuyang@gmail.com>
2022-08-07 10:44:01 -0700

    bug: fix some imgs not loading hopefully

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -1,18 +1,27 @@
 const images = document.querySelectorAll("img[data-src]");
 
-const cleanup = (event) => {
+const handleCleanup = (event) => {
   const image = event.currentTarget;
+  cleanup(image);
+};
+
+const handleLoadImage = (event) => {
+  const image = event.currentTarget;
+  loadImage(image);
+};
+
+const loadImage = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.removeEventListener("load", handleLoadImage);
+  image.addEventListener("load", handleCleanup);
+};
+
+const cleanup = (image) => {
   image.removeAttribute("data-src");
   image.classList.remove("placeholder");
 };
 
-const loadImage = (event) => {
-  const image = event.currentTarget;
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.removeEventListener("load", loadImage);
-  image.addEventListener("load", cleanup);
-};
-
 images.forEach((image) => {
-  image.addEventListener("load", loadImage);
+  if (image.complete) loadImage(image);
+  image.addEventListener("load", handleLoadImage);
 });
